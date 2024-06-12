276ca84aeef9efecf3726cb2625df8509ecade2e
Maxime Moreillon <m.moreillon@gmail.com>
2024-06-12 19:33:26 +0900

    started working on auth

---
+++ src/api/auth.ts
@@ -0,0 +1,9 @@
+type Credentials = {
+  username: string
+  password: string
+}
+
+export function login(credentials: Credentials) {
+  const { username, password } = credentials
+  return "Not implemented"
+}
---
+++ src/routes/login.tsx
@@ -0,0 +1,26 @@
+import Button from "~/components/Button"
+import Input from "~/components/Input"
+import { action, redirect, useSubmission } from "@solidjs/router"
+import { login } from "~/api/auth"
+
+const loginAction = action(async (formData: FormData) => {
+  const username = String(formData.get("username"))
+  const password = String(formData.get("password"))
+  await login({ username, password })
+  return redirect("/expressions")
+})
+
+export default function Home() {
+  return (
+    <>
+      <h1>Login</h1>
+
+      <form action={loginAction} method="post" class="my-8 flex flex-col gap-8">
+        <Input label="Username" name="username" />
+        <Input label="Password" name="password" type="password" />
+
+        <Button type="submit">Login</Button>
+      </form>
+    </>
+  )
+}
