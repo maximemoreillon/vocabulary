02ebb79e939dae60690f476a9c94c555b80f6752
Maxime Moreillon <m.moreillon@gmail.com>
2024-08-18 08:31:44 +0900

    writing unique

--- src/api/schema.ts
+++ src/api/schema.ts
@@ -2,7 +2,7 @@ import { integer, text, pgTable, serial } from "drizzle-orm/pg-core"
 
 export const expressions = pgTable("expressions", {
   id: serial("id").primaryKey(),
-  writing: text("writing"),
+  writing: text("writing").unique(),
   reading: text("reading"),
   meaning: text("meaning"),
   score: integer("score").default(0),
