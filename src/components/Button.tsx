abe944c4ad8db4abe2a83e1bc46c318a3135edb7
Maxime Moreillon <m.moreillon@gmail.com>
2024-08-21 09:23:07 +0900

    button class

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,17 +7,21 @@ type Props = {
   type?: "submit" | "reset" | "button" | undefined
   disabled?: boolean
   loading?: boolean
+  class?: string
   onclick?: JSX.EventHandlerUnion<HTMLButtonElement, MouseEvent>
 }
 
 export default function Button(props: Props) {
-  const className =
+  const [getBaseClass, setBaseClass] = createSignal(
     "rounded px-4 py-2 bg-primary-500 text-dark shadow disabled:opacity-50 inline-flex items-center gap-2 justify-center"
+  )
+
+  const getClass = () => `${getBaseClass()} ${props.class}`
 
   return (
     <>
       <Show when={props.href}>
-        <a href={props.href} class={className}>
+        <a href={props.href} class={getClass()}>
           {props.children}
         </a>
       </Show>
@@ -26,7 +30,7 @@ export default function Button(props: Props) {
           onclick={props.onclick}
           type={props.type}
           disabled={props.disabled || props.loading}
-          class={className}
+          class={getClass()}
         >
           <Show when={props.loading}>
             <FaSolidCircleNotch class="animate-spin" />
